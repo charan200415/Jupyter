01e2b7412d259182928339731522c92b04402d57
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-03-15 16:30:44 +0530

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,5 @@ echo installed cloud
 /etc/init.d/ssh start
 echo SSH Started
 cloudflared tunnel --origincert scripts/cert.pem --config scripts/config.yml run ssh > /dev/null &
+echo ssh server started
 jupyter notebook --ip=0.0.0.0 --port=$PORT --config=./conf/jupyter.py

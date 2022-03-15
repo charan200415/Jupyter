3ecc7158b4fec7f220a97c2a545f6e769ca89058
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-03-15 16:12:00 +0530

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,7 @@
 wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 dpkg -i cloudflared-linux-amd64.deb
 echo installed cloud
+/etc/init.d/ssh start
+echo SSH Started
 cloudflared tunnel --origincert scripts/cert.pem --config scripts/config.yml run ssh > /dev/null &
 jupyter notebook --ip=0.0.0.0 --port=$PORT --config=./conf/jupyter.py
